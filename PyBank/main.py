095c686cf353a598c3154b86b059f72f8a2aabaa
Sans <77713458+Sanskruti1982@users.noreply.github.com>
2021-02-24 21:35:05 +1100

    Final Version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,9 @@
 import os
 import csv
-
 #Path to collect data from PyBank folder
 csvpath = os.path.join("budget_data.csv")
-
 #Lists to store month data
 month = [0]
-
-
 #Read in the CSV file
 with open(csvpath,'r') as csvfile:
     #Split the data on commas
@@ -16,8 +12,6 @@ with open(csvpath,'r') as csvfile:
     header = next(csvreader)
     #Determine first row for profit/loss value
     firstrow = next(csvreader)
-   
-    
     #Adding the variables to for change and greatest increase and decrease
     totalchange = 0
     changesum = 0
@@ -28,7 +22,6 @@ with open(csvpath,'r') as csvfile:
     #Defining the first row as a variable and total also as first row
     previousvalue = int(firstrow[1])
     total = previousvalue
-    
    #Looping through the data to get values
     for row in csvreader:
         total = total + int(row[1])
@@ -41,7 +34,6 @@ with open(csvpath,'r') as csvfile:
         totalchange = totalchange + change
         #Defining previous value as the next row so that the next row is always the one row down in the matrix
         previousvalue = int(row[1])
-        
         #Defining maximum and minimum increase
         #Getting the maximum of the change and getting the corresponding to month
         if change > maxinc:
@@ -51,23 +43,19 @@ with open(csvpath,'r') as csvfile:
         if change < mininc:
             mininc = change
             minincmonth = row[0]
-        
     #Averaging the change over the period
     #Taking one away from the length as the first change is zero and this length accounts for the count of months
     avgchange = totalchange/(length-1)
-  
 #Creating a text file to print the final output result
 output_file = os.path.join("final.txt")
-
 with open(output_file, "w") as datafile:
     print("Financial Analysis", file=datafile)
     print("----------------------------", file=datafile)
-    print("Total months: " + str(length), file=datafile)
+    print("Total Months: " + str(length), file=datafile)
     print("Total: " + "$" + str(total), file=datafile)
-    print("Average Change: " + "$" + str(round(avgchange,2)))
+    print("Average Change: " + "$" + str(round(avgchange,2)), file=datafile)
     print("Greatest Increase in Profits: " +  str(maxincmonth) + " (" + "$" + str(maxinc) + ")", file=datafile)
     print("Greatest Decrease in Profits: " +  str(minincmonth) + " (" + "$" + str(mininc) + ")", file=datafile)
-
 #Printing the analysis on to the terminal
 print("Financial Analysis")
 print("----------------------------")
